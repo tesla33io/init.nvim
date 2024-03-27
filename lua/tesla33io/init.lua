83f8854456bbcc2b0a9e31e7ff32955d23c2afa2
astavrop <artemstavropolov@gmail.com>
2024-03-27 15:40:10 +0100

    chore: some refactoring

--- lua/tesla33io/init.lua
+++ lua/tesla33io/init.lua
@@ -1,3 +1,3 @@
 require("tesla33io.remap")
+require("tesla33io.h_protect")
 require("tesla33io.set")
-
--- lua/tesla33io/packer.lua
+++ lua/tesla33io/packer.lua
@@ -4,48 +4,55 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.5',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.5',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
 
-  -- Colorscheme KANAGAWA.nvim
-  use ({
-	  "rebelot/kanagawa.nvim",
-	  as = "kanagawa",
-	  config = function()
-		  vim.cmd("colorscheme kanagawa")
-	  end
-  })
+	-- Colorscheme KANAGAWA.nvim
+	use ({
+		"rebelot/kanagawa.nvim",
+		as = "kanagawa",
+		config = function()
+			vim.cmd("colorscheme kanagawa")
+		end
+	})
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use('nvim-treesitter/playground')
-  use('ThePrimeagen/harpoon')
-  use('mbbill/undotree')
-  use('tpope/vim-fugitive')
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v3.x',
-	  requires = {
-		  --- Uncomment the two plugins below if you want to manage the language servers from neovim
-		  -- {'williamboman/mason.nvim'},
-		  -- {'williamboman/mason-lspconfig.nvim'},
+	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+	use('nvim-treesitter/playground')
+	use('ThePrimeagen/harpoon')
+	use('mbbill/undotree')
+	use('tpope/vim-fugitive')
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v3.x',
+		requires = {
+			--- Uncomment the two plugins below if you want to manage the language servers from neovim
+			-- {'williamboman/mason.nvim'},
+			-- {'williamboman/mason-lspconfig.nvim'},
 
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'L3MON4D3/LuaSnip'},
-	  }
-  }
-  use {
-	  "williamboman/mason.nvim"
-  }
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},
+			{'hrsh7th/cmp-nvim-lsp'},
+			{'L3MON4D3/LuaSnip'},
+		}
+	}
+	use {
+		"williamboman/mason.nvim"
+	}
+	use {
+		'glepnir/nerdicons.nvim',
+		cmd = 'NerdIcons',
+		config = function()
+			require('nerdicons').setup({})
+		end
+	}
 
 end)
 
